bf8aebeeba51eef3184ebed9b613823cfecacef7
Roman Suhoniak <evil.arthass67@gmail.com>
2024-06-12 15:25:11 +0300

    Printing message "hello world" 10 times

---
+++ main.kt
@@ -0,0 +1,16 @@
+fun main() {
+    repeat(10) {
+        printMessage()
+    }
+    vivaMechMat()
+}
+
+fun printMessage() {
+    println("Hello, World")
+}
+
+fun vivaMechMat() {
+    repeat(10) {
+        println("Viva Mech-mat faculty")
+    }
+}
